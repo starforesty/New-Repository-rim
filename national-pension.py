556c6c9d3a15d962c19350f83b33d4e637242916
kyurim <starforesty@gmail.com>
2026-04-09 12:21:44 +0900

    Update national-pension.py

--- national-pension.py
+++ national-pension.py
@@ -73,7 +73,7 @@ class PensionData():
 
 @ st.cache_data     # 웹 서비스가 새로고침 될 때 이 부분 빼고 기능만 돌아감 - 이거 없으면 새로고침 될 때마다 전처리부터 다시 시작
 def read_pensiondata():
-    data = PensionData('https://drive.google.com/file/d/1itux9CgrEj7oJSXIgJZiaXhn4yfdfPYD/view?usp=drive_link')
+    data = PensionData('https://drive.google.com/uc?export=download&id=1itux9CgrEj7oJSXIgJZiaXhn4yfdfPYD')
     return data
 
 data = read_pensiondata()
